f1ea0753ce89a14736f5a362cf6efabfe24b61f2
shivrajsingh-tech <shivrajsingh17773@gmail.com>
2026-03-11 21:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (34.80%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> countStack;
+        stack<string> stringStack;
+        string curr = "";
+        int num = 0;
+
+        for (char ch : s) {
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+            else if (ch == '[') {
+                countStack.push(num);
+                stringStack.push(curr);
+                num = 0;
+                curr = "";
+            }
+            else if (ch == ']') {
+                int repeat = countStack.top();
+                countStack.pop();
+
+                string temp = stringStack.top();
+                stringStack.pop();
+
+                while (repeat--) {
+                    temp += curr;
+                }
+                curr = temp;
+            }
+            else {
+                curr += ch;
+            }
+        }
+        return curr;
+    }
+};
